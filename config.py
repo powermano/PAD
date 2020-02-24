6ebed44c5f300ec92678e76a6a7e2bbe8186a039
tao.cai <tao.cai@gpu-dev005.hogpu.cc>
2020-02-24 20:14:57 +0800

    add config

--- config.py
+++ config.py
@@ -9,20 +9,20 @@ def get_config(training = True):
     conf.eval = edict()
 
 
-    conf.gpu = 3
+    conf.gpu = 0
     conf.use_concat = False
     conf.multi_output = True
     conf.add = True
     conf.feature_c = 128
     conf.use_triplet = True
     conf.use_officical_resnet18 = False
-    conf.triplet_ratio = 0.001
+    conf.triplet_ratio = 0.01
     conf.triplet_margin = 0.2
     conf.print_freq = 20
     conf.rgb = True
     conf.crop = False
 
-    conf.data_folder = '/mnt/cephfs/smartauto/users/guoli.wang/jiachen.xue/anti_spoofing/data/CASIA-CeFA/phase2' #data root for training, and testing, you should change is according to your setting
+    conf.data_folder = '/mnt/cephfs/smartauto/users/guoli.wang/jiachen.xue/anti_spoofing/data/CASIA-CeFA/phase1' #data root for training, and testing, you should change is according to your setting
     conf.save_path = '/mnt/cephfs/smartauto/users/guoli.wang/tao.cai/cvpr_model'
     #conf.save_path = './work_space/save' #path for save model in training process, you should change it according to your setting
     conf.train_list =  '/mnt/cephfs/smartauto/users/guoli.wang/jiachen.xue/anti_spoofing/data/CASIA-CeFA/phase1/4@3_train.txt' #path where training list is, you should change it according to your setting
@@ -30,7 +30,7 @@ def get_config(training = True):
     conf.test_list = '/mnt/cephfs/smartauto/users/guoli.wang/jiachen.xue/anti_spoofing/data/CASIA-CeFA/phase2/4@3_test_res.txt'
    # conf.test_list = '/mnt/cephfs/smartauto/users/guoli.wang/jiachen.xue/anti_spoofing/data/CASIA-CeFA/phase1/4@3_dev_res.txt' #path where test list is, you should change it according to your setting
     conf.batch_size = 128
-    conf.exp = 'phase1_4@3_lr_0.001_epoch_30_input_size_384_emd_128_dropout_0.0_triplet_ratio_0.001' #model is saved in conf.save_path/conf.exp, if you want to train different models, you can distinguish them according to this parameter
+    conf.exp = 'phase1_4@3_lr_0.001_epoch_30_input_size_384_emd_128_dropout_0.0_triplet_ratio_0.01_fix_wo_se' #model is saved in conf.save_path/conf.exp, if you want to train different models, you can distinguish them according to this parameter
    # conf.exp = 'phase1_4@3_lr_0.001_epoch_30_input_size_384_emd_128_dp_0_concat_128_bs_128_crop' #model is saved in conf.save_path/conf.exp, if you want to train different models, you can distinguish them according to this parameter
    # conf.exp = 'phase1_4@1_resnet18_lr_0.001_epoch_30_input_size_384_emd_128_bs_128'
    # conf.exp = 'phase1_4@1_lr_0.001_epoch_30_input_size_512_emd_128_bs_64'
@@ -42,7 +42,7 @@ def get_config(training = True):
 
     conf.model.input_size = [384,384] #the input size of our model
     conf.model.random_offset = [0,0] #for random crop
-    conf.model.use_senet = True #senet is adopted in our resnet18 model
+    conf.model.use_senet = False #senet is adopted in our resnet18 model
     conf.model.se_reduction = 16 #parameter concerning senet
     conf.model.drop_out = 0.0 #we add dropout layer in our resnet18 model
     conf.model.embedding_size = 128 #feature size of our resnet18 model
